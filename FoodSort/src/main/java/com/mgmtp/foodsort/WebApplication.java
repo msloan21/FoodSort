2a0e14fbc0f2031e9ded7cd45810becc6bd54aae
MSloan <msloan21@gmail.com>
2020-06-03 19:11:37 -0400

    address CORS

--- FoodSort/src/main/java/com/mgmtp/foodsort/WebApplication.java
+++ FoodSort/src/main/java/com/mgmtp/foodsort/WebApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @ServletComponentScan
 @SpringBootApplication(scanBasePackages = "com.mgmtp.foodsort")
